b8ef04d6ea33075ec4299fb586baaea6b9b1da49
harshanp2003 <harshanp603@gmail.com>
2025-07-21 12:17:01 +0530

    First Commit

---
+++ RandomApp/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace RandomApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
